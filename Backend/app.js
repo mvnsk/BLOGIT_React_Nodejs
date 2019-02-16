f74e4c00d24f287f4bdf8f83dffb41595152192a
karthikmudunuru <mudunuru@usc.edu>
2019-02-16 11:13:29 -0800

    ready for deployment

---
+++ Backend/app.js
@@ -0,0 +1,98 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require("uuid");
+var cors = require('cors')
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+
+
+
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'images');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().toISOString() + '-' + file.originalname);
+//   }
+// });
+const fileStorage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+      cb(null, uuidv4());
+  }
+});
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//       cb(null, 'images');
+//   },
+//   filename: (req, file, cb) => {
+//       cb(null, new Date().toISOString().replace(/:/g, '-') + '-'  + file.originalname);
+//   }
+// });
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://mudunuru:mongopassword@cluster0.g9j3j.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+ 
+    io.on('connection', socket => {
+     
+      console.log('Client connected');
+    });
+    io.use()
+  })
+  .catch(err => console.log(err));
